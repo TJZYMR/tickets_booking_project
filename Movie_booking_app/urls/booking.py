eb29241ada01fd181e8fd3a0f6c2fa3ef4a965cb
Tatva Joshi <tatva.joshi@zymr.com>
2022-07-29 18:56:06 +0530

    auth added

---
+++ Movie_booking_app/urls/booking.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+urlpatterns = []
+# --------------------------------------------------
+# 1. booking a movie(which will include payments,notifications,)
+# --------------------------------------------------
+# 2.cancel a booking
